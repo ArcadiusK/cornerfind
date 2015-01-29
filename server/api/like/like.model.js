1d3b152108b835246a6fa71d9be0b722050ed5ee
Arcadius Kazimierski <arcadiusk@gmail.com>
2015-01-29 13:34:51 -0500

    fixed double curly brackets

--- server/api/like/like.model.js
+++ server/api/like/like.model.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var LikeSchema = new Schema({
-  productId: {{type: Schema.Types.ObjectId, ref: 'Product', index:true}},
-  userId: {{type: Schema.Types.ObjectId, ref: 'User', index:true}},
+  productId: {type: Schema.Types.ObjectId, ref: 'Product', index:true},
+  userId: {type: Schema.Types.ObjectId, ref: 'User', index:true},
 
 });
 
