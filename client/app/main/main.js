3882fdade31e1462c0bfec7e8fa97388bd165acb
Arcadius Kazimierski <a@Arcadiuss-MacBook-Pro.local>
2015-01-29 02:39:20 -0500

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cornerfindApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
