f2d503a6612e542f84705ce6e9a87ba994ffce55
justincoh <justincoh@gmail.com>
2015-01-29 11:38:46 -0500

    Added seller Id, productId is now array

--- server/api/order/order.model.js
+++ server/api/order/order.model.js
@@ -4,10 +4,11 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var OrderSchema = new Schema({
-  productId: {type: Schema.Types.ObjectId, ref: 'Product'},
+  productId: [{type: Schema.Types.ObjectId, ref: 'Product'}],
+  sellerId: {type: Schema.Types.ObjectId, ref: 'User'},
   buyerId: {type: Schema.Types.ObjectId, ref: 'User'},
   price: {type: Number, min: 0},
-  status: {type:String,enum: ['offer','accepted','shipped','received','issues']}
+  status: {type:String, enum: ['offer','accepted','shipped','received','issues']}
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
