d72304209cadbc02eb333cca2191718cffe3fbee
justincoh <justincoh@gmail.com>
2015-01-29 11:10:23 -0500

    Order Schema Created

---
+++ server/api/order/order.model.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var OrderSchema = new Schema({
+  productId: {type: Schema.Types.ObjectId, ref: 'Product'},
+  buyerId: {type: Schema.Types.ObjectId, ref: 'User'},
+  price: {type: Number, min: 0},
+  status: {type:String,enum: ['offer','accepted','shipped','received','issues']}
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
