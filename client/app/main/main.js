151045f7b31070d3094591c4f78be29b68b476c7
muchimoto <muchimoto@gmail.com>
2015-01-30 12:44:01 -0500

    implemented user service

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,10 +8,6 @@ angular.module('cornerfindApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       }) 
-      // .state('profile', {
-      //   url: '/profile',
-      //   templateUrl: 'app/profile/profile.html',
-      //   controller: 'ProfileCtrl'
-      // });
+
 
   });
