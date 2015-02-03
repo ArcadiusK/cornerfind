17c0df5ee36231e0ca84c0eb3b9258b1644f4d05
David Chang <david.j.chang@gmail.com>
2015-02-02 22:57:13 -0500

    Prior to AddChat closes #30

--- client/app/chat/chat.service.js
+++ client/app/chat/chat.service.js
@@ -10,19 +10,20 @@ angular.module('cornerfindApp')
     return {
       getChatList: function () {
         var deferred = $q.defer();
-        $http.get('/api/chats/' + '54cffa234d3feb28f948c864')
+
+        $http.get('/api/chats/' + '54d001dfae377bf2fde2cc61')
         .success(function(results) {
           deferred.resolve(results)
         }).error(function() {
           console.log('Chat Get Failed');
-       });
-      return deferred.promise;
+        });
+        return deferred.promise;
       // socket.syncUpdates('chat', $scope.chatlist);
       },
       
       addChat: function(newChat) {
-        chatList = chatList.push(newChat);
-        console.log(chatList);
+        console.log(newChat);
+        $http.post('/api/chats/', {newChat: newChat})
       }
     };
 
