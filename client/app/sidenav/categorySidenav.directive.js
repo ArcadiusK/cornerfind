c5ca412406bd037e22d506189ecaae18ee5e7502
justincoh <justincoh@gmail.com>
2015-02-03 17:35:28 -0500

    Lots of sidenavs, add product working

--- client/app/sidenav/categorySidenav.directive.js
+++ client/app/sidenav/categorySidenav.directive.js
@@ -1,21 +1,18 @@
 'use strict';
 
 angular.module('cornerfindApp')
-    .directive('sidenav', function($mdSidenav) {
+    .directive('categorysidenav', function($mdSidenav) {
         return {
-            templateUrl: 'app/sidenav/sidenav.html',
+            templateUrl: 'app/sidenav/categorySidenav.html',
             restrict: 'EA',
             scope: {selection:'=',
         			onItemClick: "&",
-        			buttonText :'@',
-                    sidebarid: '@'
+        			buttonText :'@'
         			},
             link: function(scope, element, attrs) {
                   scope.id = scope.sidebarid;
             	  scope.openLeftMenu = function() {
-                   console.log('AAAA',scope.sidebarid);
-                     console.log($mdSidenav())
-                    $mdSidenav(scope.sidebarid).toggle();
+                    $mdSidenav("categories").toggle();
                     
                 };
 
--- client/app/users/add_product/add_product.controller.js
+++ client/app/users/add_product/add_product.controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('cornerfindApp')
-    .controller('AddProductCtrl', function($scope, brand, category, $q, products, $mdSidenav, Auth) {
+    .controller('AddProductCtrl', function($scope,brand,condition, category, $q, products, $mdSidenav, Auth) {
 
 
         //GET all Brands and Categories
         $scope.availableBrands = brand.query();
         $scope.availableCategories = category.query();
-
+        $scope.availableConditions = condition.query();
         //Add a new product
         $scope.newProduct = {
             category: []
@@ -19,90 +19,82 @@ angular.module('cornerfindApp')
         $scope.showCategories = false;
 
         $scope.selectedBrand = function(selected) {
-            // $scope.showBrands = true;
-            // $scope.showCategories = false;
-            console.log('SELECTED ', selected)
-            $scope.newProduct["brand"] = selected._id;
-
+            $scope.newProduct.brand = selected._id;
         }
 
         $scope.selectedCategory = function(selected) {
-            // $scope.showCategories = true;
-            // $scope.showBrands = false;
-            console.log('SELECTED ', selected)
             if ($scope.newProduct.category.indexOf(selected._id) !== -1) return;
-            $scope.newProduct["category"].push(selected._id);
+            $scope.newProduct.category.push(selected._id);
         }
 
+        $scope.selectedCondition = function(selected) {
+            $scope.newProduct.condition = selected._id;
+        }
+     
 
-        // $scope.openLeftMenu = function() {
-
-        //     $mdSidenav('left').toggle();
-        // };
-
-
-
-
-
-
-
-
-
-        $scope.addProduct = function() {
-
-
-
-            $scope.currentUser = Auth.getCurrentUser();
-            $scope.tagObjects = [];
-            $scope.products = [];
-            $scope.availableCategories = category.query();
-
-
-            // brand.query();
-
-            if ($scope.name === '') return;
-            if ($scope.price === '') return;
-            if ($scope.description === '') return;
-
-            //CREATING NEW TAG OBJECTS
-
-            //For each tag in scope, call Tags.save and create an array of $q promises.
-            var promises = [];
-
-            angular.forEach($scope.tags, function(tag) {
-                var promise = Tags.save({
-                    name: tag
-                });
-                promises.push(promise.$promise);
+        $scope.addProduct=function(newProduct){
+            products.save(newProduct,function(){
+                console.log('Save Callback ',arguments)
             });
-            var tagsPromise = $q.all(promises);
-
-            tagsPromise.then(function(tags) {
-
-                $scope.tagObjects = tags.map(function(tag) {
-                    return tag._id
-                });
-
-                //Saving new product in DB
-                products.save({
-                    name: $scope.name,
-                    desc: $scope.description,
-                    price: $scope.price,
-                    category: $scope.category,
-                    userId: $scope.currentUser._id,
-                    media: $scope.product_images,
-                }, function(err, product) {
-                    if (err) {
-                        // console.log(err)
-                    };
-
-                    $scope.product = product;
-                })
-
-            })
-
-
         }
+
+
+
+        // $scope.addProduct = function() {
+
+
+
+        //     $scope.currentUser = Auth.getCurrentUser();
+        //     $scope.tagObjects = [];
+        //     $scope.products = [];
+        //     $scope.availableCategories = category.query();
+
+
+        //     // brand.query();
+
+        //     if ($scope.name === '') return;
+        //     if ($scope.price === '') return;
+        //     if ($scope.description === '') return;
+
+        //     //CREATING NEW TAG OBJECTS
+
+        //     //For each tag in scope, call Tags.save and create an array of $q promises.
+        //     var promises = [];
+
+        //     angular.forEach($scope.tags, function(tag) {
+        //         var promise = Tags.save({
+        //             name: tag
+        //         });
+        //         promises.push(promise.$promise);
+        //     });
+        //     var tagsPromise = $q.all(promises);
+
+        //     tagsPromise.then(function(tags) {
+
+        //         $scope.tagObjects = tags.map(function(tag) {
+        //             return tag._id
+        //         });
+
+        //         //Saving new product in DB
+        //         products.save({
+        //             name: $scope.name,
+        //             desc: $scope.description,
+        //             price: $scope.price,
+        //             category: $scope.category,
+        //             userId: $scope.currentUser._id,
+        //             media: $scope.product_images,
+        //         }, function(err, product) {
+        //             if (err) {
+        //                 // console.log(err)
+        //             };
+
+        //             $scope.product = product;
+        //         })
+
+        //     })
+
+
+        // }
 
 
 
