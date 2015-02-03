f1cf5075ed5daa64347380c535fd6ac056adf89d
justincoh <justincoh@gmail.com>
2015-02-03 18:36:59 -0500

    add product page functioning

--- client/app/users/add_product/add_product.controller.js
+++ client/app/users/add_product/add_product.controller.js
@@ -3,21 +3,14 @@
 angular.module('cornerfindApp')
     .controller('AddProductCtrl', function($scope,brand,condition, category, $q, products, $mdSidenav, Auth) {
 
-
         //GET all Brands and Categories
         $scope.availableBrands = brand.query();
         $scope.availableCategories = category.query();
         $scope.availableConditions = condition.query();
+
         //Add a new product
-        $scope.newProduct = {
-            category: []
-        };
-
-        $scope.newProductDisplay ={
-              category: []
-        };
-
-        
+        $scope.newProduct = {category: []};
+        $scope.newProductDisplay ={category: []};
 
         $scope.showBrands = false;
         $scope.showCategories = false;
@@ -50,67 +43,6 @@ angular.module('cornerfindApp')
                 console.log('Save Callback ',arguments)
             });
         }
-
-
-
-        // $scope.addProduct = function() {
-
-
-
-        //     $scope.currentUser = Auth.getCurrentUser();
-        //     $scope.tagObjects = [];
-        //     $scope.products = [];
-        //     $scope.availableCategories = category.query();
-
-
-        //     // brand.query();
-
-        //     if ($scope.name === '') return;
-        //     if ($scope.price === '') return;
-        //     if ($scope.description === '') return;
-
-        //     //CREATING NEW TAG OBJECTS
-
-        //     //For each tag in scope, call Tags.save and create an array of $q promises.
-        //     var promises = [];
-
-        //     angular.forEach($scope.tags, function(tag) {
-        //         var promise = Tags.save({
-        //             name: tag
-        //         });
-        //         promises.push(promise.$promise);
-        //     });
-        //     var tagsPromise = $q.all(promises);
-
-        //     tagsPromise.then(function(tags) {
-
-        //         $scope.tagObjects = tags.map(function(tag) {
-        //             return tag._id
-        //         });
-
-        //         //Saving new product in DB
-        //         products.save({
-        //             name: $scope.name,
-        //             desc: $scope.description,
-        //             price: $scope.price,
-        //             category: $scope.category,
-        //             userId: $scope.currentUser._id,
-        //             media: $scope.product_images,
-        //         }, function(err, product) {
-        //             if (err) {
-        //                 // console.log(err)
-        //             };
-
-        //             $scope.product = product;
-        //         })
-
-        //     })
-
-
-        // }
-
-
-
 
         $scope.upload = function(thing) {
 
