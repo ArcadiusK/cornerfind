9f226fd96c7f17517f5a7f5ad9df3aea65aa6cd7
justincoh <justincoh@gmail.com>
2015-02-04 17:41:22 -0500

    fixed spacing in order model

--- server/api/order/order.model.js
+++ server/api/order/order.model.js
@@ -4,12 +4,14 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var OrderSchema = new Schema({
-  lineItems: [{ productId: {type: Schema.Types.ObjectId, ref: 'Product'},
-                name: {type: String},
-                purchasePrice: {type: Number, required: true,min: 0},
-                qty: {type: Number, min: 1, default:1},
-                shippingHandling: {type: Number, default:499}
-             }],
+  lineItems: [
+    { productId: {type: Schema.Types.ObjectId, ref: 'Product'},
+      name: {type: String},
+      purchasePrice: {type: Number, required: true,min: 0},
+      qty: {type: Number, min: 1, default:1},
+      shippingHandling: {type: Number, default:499}}
+    ],
+    
   sellerId: {type: Schema.Types.ObjectId, ref: 'User'},
   buyerId: {type: Schema.Types.ObjectId, ref: 'User'},
   orderShippingHandling: {type: Number},
@@ -28,6 +30,4 @@ OrderSchema.pre('save',function(next){
   next();
 });
 
-
-//hook to send offer to the list of user offers in user schema
-//also need a setter for the status enum
+//need a setter for the status enum
