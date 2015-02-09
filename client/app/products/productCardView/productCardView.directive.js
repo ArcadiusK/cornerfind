970a3481901e7ce42b9c74368b07646702ac5c98
David Chang <david.j.chang@gmail.com>
2015-02-08 20:16:38 -0500

    closes #125

--- client/app/products/productCardView/productCardView.directive.js
+++ client/app/products/productCardView/productCardView.directive.js
@@ -11,8 +11,6 @@ angular.module('cornerfindApp')
       },
       link: function (scope, element, attrs) {
 
-        console.log(scope.product);
-
         likes.resource.getProductLikes({id: scope.product._id}).$promise.then(function(data){
           
           scope.product.likes = data;
@@ -45,77 +43,31 @@ angular.module('cornerfindApp')
 
         });
 
-
-      
-
       
         //toggle favorite function to update backend. 
         scope.toggleFavorite = function(){
+          var likeObject = {productId: scope.product._id, userId: scope.currentUser._id};
+          
           // If already favorited do this
-          if (scope.currentUser.likes.indexOf(scope.product._id) > -1) {
-            console.log('remove from favorite', scope.product._id);
-            scope.currentUser.likes.splice(scope.currentUser.likes.indexOf(scope.product._id), 1);
-            scope.product.likes.splice(scope.product.likes.indexOf(scope.currentUser._id), 1);
+          if (scope.favorited) {
+             
+            var userLikeIndex = scope.currentUser.likes.map(function(e) { return e.productId; }).indexOf(scope.product._id);
+            scope.currentUser.likes.splice(userLikeIndex, 1);
+
+            var productLikeIndex = scope.product.likes.map(function(e) { return e.userId; }).indexOf(scope.currentUser._id);
+            scope.product.likes.splice(productLikeIndex, 1);
+            
             likes.resource.deleteLike({productid: scope.product._id, userid: scope.currentUser._id});
             scope.favorited = false;
           }
-
+          // If not favorited do this
           else{
-            console.log('adding to favorite', scope.product._id);
-            scope.currentUser.likes.push(scope.product._id);
-            scope.product.likes.push(scope.currentUser._id);
+            scope.currentUser.likes.push({productId: scope.product._id, userId: scope.currentUser._id});
+            scope.product.likes.push({productId: scope.product._id, userId: scope.currentUser._id});
             likes.resource.save({productId: scope.product._id, userId: scope.currentUser._id});
             scope.favorited = true;
           }
-            
-
-            
-
-        //     scope.currentUser.following.splice($scope.currentUser.following.indexOf($scope.user._id), 1)
-                
-        //         $timeout(function() {
-        //             User.update($scope.currentUser)
-        //                 .$promise.then(function(user) {
-        //                     console.log('User Unfollowed! New logged in user.followed is ..', user)
-        //                     $scope.followed = false;
-        //                     $scope.toggleText = 'Follow';
-        //                     $scope.$apply;
-        //                 });
-        //         }, 1000);
-
-
-        //         $scope.user.followers.splice($scope.user.followers.indexOf($scope.currentUser._id), 1)
-        //             // Update the user of the page's followers array
-        //         User.update($scope.user)
-        //             .$promise.then(function(user) {
-        //                 console.log('Follower removed from users followers array ..', user)
-        //             });
-
-        //     }
-        //     // If not following yet do this
-        //     else {
-
-        //         // Update logged in user's following Array
-        //         $scope.currentUser.following.push($scope.user._id)
-        //         User.update($scope.currentUser)
-        //             .$promise.then(function(user) {
-        //                 console.log('User Followed! New logged in user.followed is ..', user)
-        //                 $scope.followed = true;
-        //                 $scope.toggleText = 'Following';
-        //                 $scope.$apply;
-        //             });
-
-        //         // Update the user of the page's followers array
-        //         $scope.user.followers.push($scope.currentUser._id)
-        //         User.update($scope.user)
-        //             .$promise.then(function(user) {
-        //                 console.log('Follower pushed to users followers array ..', user)
-        //             });
-
-        //     }
-        // }
-
-        
+          
 
       }
     }
