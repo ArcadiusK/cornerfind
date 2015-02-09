7e44d9a17514b6d73d6a968d4cf4f59fad039770
David Chang <david.j.chang@gmail.com>
2015-02-09 16:56:40 -0500

    closes #115

--- server/api/product/s3_upload.js
+++ server/api/product/s3_upload.js
@@ -5,8 +5,8 @@ var secrets = require('../../config/environment');
 
 var s3 = new AWS.S3();
 AWS.config.update({
-        process.env.AWS_ACCESS_KEY_ID,
-        process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 //A sample route to receive
