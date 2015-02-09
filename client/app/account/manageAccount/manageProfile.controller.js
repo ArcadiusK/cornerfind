e7f9a954059eecd20a532e4451d249e0b0e85985
justincoh <justincoh@gmail.com>
2015-02-09 13:28:40 -0500

    Editing user info works

--- client/app/account/manageAccount/manageProfile.controller.js
+++ client/app/account/manageAccount/manageProfile.controller.js
@@ -1,30 +1,18 @@
 angular.module('cornerfindApp')
 	.controller('ManageProfileCtrl',function($scope, Auth, User){
-		$scope.resetUserInfo = function(){
-			$scope.currentUser = Auth.getCurrentUser();
-			console.log('Why doesnt this live update the inputs?')
-		}
-
-
-		$scope.toggleSuccess = function(){
-			if(typeof $scope.success==='undefined'|| $scope.success ===false){
-				$scope.success=true;
-			}
-			else {
-				$scope.success =false;
-				$scope.$apply();
-			}
-		}
-		$scope.successPopup = function(){
-			$scope.toggleSuccess()
-			var changeBack = _.debounce($scope.toggleSuccess,3000)
-			changeBack();
-		}
+		$scope.currentUser = Auth.getCurrentUser();
+		$scope.master ={};
+		//For form Reset
+		angular.copy($scope.currentUser,$scope.master)
 
 		$scope.updateUser = function(userObject){
 			User.update(userObject._id,userObject,function(res){
-				$scope.successPopup();
+				$scope.success = true;
 			})
+		}
+
+		$scope.cancelChanges = function(){
+			angular.copy($scope.master,$scope.currentUser)
 		}
 
 	})
