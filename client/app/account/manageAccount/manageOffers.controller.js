02a767e0c212b19061fe2772e3330af72b53ba47
justincoh <justincoh@gmail.com>
2015-02-09 22:16:25 -0500

    edited offer management card

--- client/app/account/manageAccount/manageOffers.controller.js
+++ client/app/account/manageAccount/manageOffers.controller.js
@@ -4,6 +4,8 @@ angular.module('cornerfindApp')
 	.controller('ManageOffersCtrl',function($scope, offer){
 		$scope.offers = offer.manageOffers({id:$scope.currentUser._id})
 		
+		// if($scope.offers.length ===0)
+
 		$scope.acceptOffer = function(orderId){
 			offer.acceptOffer({id:orderId})
 		}
