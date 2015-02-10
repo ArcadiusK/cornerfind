dbfab39a1e3af9265e019f44e84b0b71320f5aa4
muchimoto <muchimoto@gmail.com>
2015-02-10 15:09:48 -0500

    buyNow button now working

--- client/app/account/manageAccount/manageOffers.controller.js
+++ client/app/account/manageAccount/manageOffers.controller.js
@@ -7,27 +7,29 @@ angular.module('cornerfindApp')
         }, function(offer) {
 
           // Create digestible stripe order
-            
+           
             $scope.stripeOrder = {
                     stripeToken: offer[0].buyerId.billing.stripeToken,
                     orderTotal: offer[0].lineItems[0].purchasePrice
             }
              
-            console.log('scope.stripeOrder is...', $scope.stripeOrder);
         })
 
-
-
+        $scope.stripeResult = {};
 
         $scope.acceptOffer = function(orderId) {
           
             offer.acceptOffer({
                 id: orderId
             }, function(result) {
-                console.log('result of acceptOffer is ..', result)
+
             });
             offer.charge($scope.stripeOrder, function(result) {
-                console.log('result of charge is ..', result)
+            
+                if (result.captured === true) {
+                $scope.stripeResult = result;
+               
+                }
             });
         }
 
