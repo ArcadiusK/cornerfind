68459f1e5053baf203e1c653ebc8d13c221e5407
David Chang <david.j.chang@gmail.com>
2015-02-10 20:01:39 -0500

    asd;lfjas;dlfk

--- client/app/easypost/easypost.controller.js
+++ client/app/easypost/easypost.controller.js
@@ -3,11 +3,20 @@
 angular.module('cornerfindApp')
   .controller('EasypostCtrl', function ($scope, $http) {
    $scope.verifyAddyResult;
+	$scope.false = true;
 
-   $scope.verifyAddress = function(address){
-   		console.log(address);
+   $scope.saveAddress = function(address){
+   		$scope.addyConfirm = true;
+   		toast('We found a match! Please review, modify and save', 8000) // 4000 is the duration of the toast
 	   	$http.post('/api/easyposts/verify', {fromAddress: address}).success(function(results){
-			$scope.verifyAddyResult = results.address;
+			$scope.address = results.address;
+		});
+   }
+
+   $scope.createLabel = function(buyerAddress, sellerAddress){
+ 	   	console.log(buyerAddress, SellerAddress);
+	   	$http.post('/api/easyposts/createLabel', {to_Address: buyerAddress, from_address: sellerAddress}).success(function(results){
+			$scope.labelURL = results;
 		});
    }
 
