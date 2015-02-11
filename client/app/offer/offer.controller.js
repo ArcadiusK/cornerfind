0963edd069c1edc6a130ab599df5871389efc892
justincoh <justincoh@gmail.com>
2015-02-10 20:29:29 -0500

    Removed old cart stuff, rebuilt offer tracking page with cards

--- client/app/offer/offer.controller.js
+++ client/app/offer/offer.controller.js
@@ -1,15 +1,22 @@
 'use strict';
 
 angular.module('cornerfindApp')
-  .controller('OfferCtrl', function ($scope, $stateParams, offer) {
+  .controller('OfferCtrl', function ($scope, $stateParams, offer, Auth) {
     $scope.userId = $stateParams.userId;	//User Id is stored here
 
-    $scope.offers = offer.getBuyersOffers({id:$scope.userId});
+
+
+    $scope.offers = offer.getBuyersOffers({id:Auth.getCurrentUser()._id});
     
-    $scope.removeOffer = function(param){
+    $scope.cancelOffer = function(param){
     	offer.delete({id:param._id},function(res,err){
     		var index = $scope.offers.indexOf(param);
     		$scope.offers.splice(index,1);
     	})
     }
+
+    $scope.modifyOffer = function(){
+        
+    }
+
   });
