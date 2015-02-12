d041ebe77082c7029c050b4d56b3e9304c8bf85e
justincoh <justincoh@gmail.com>
2015-02-12 12:49:32 -0500

    WOrking

--- server/api/address/address.model.js
+++ server/api/address/address.model.js
@@ -7,7 +7,7 @@ var AddressSchema = new Schema({
   userId: {type: Schema.Types.ObjectId, ref: 'User', index:true},
   name: String, //added in to make dealing with easypost easier
   phone: String, //added in to make dealing with easypost easier
-  billingTrueOrShippingFalse: {type: Boolean},
+  billing: {type: Boolean,default:false},
   street1: {type: String},
   street2: {type: String},
   city: {type: String},
