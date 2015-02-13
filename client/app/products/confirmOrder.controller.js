fe5028092ff1c079e5262636a32ae5036b568b85
justincoh <justincoh@gmail.com>
2015-02-13 10:21:57 -0500

    Refactored checkout working, needs styling and modification options

--- client/app/products/confirmOrder.controller.js
+++ client/app/products/confirmOrder.controller.js
@@ -7,10 +7,32 @@ angular.module('cornerfindApp')
 		//if get order returns things, add the cookie
 		//otherwise pull the cookie that's already there
 
+		//question  cookies vs factory object for temporary data store
+		//what about refreshes?
 		if($scope.confirmOrder.hasOwnProperty('lineItems')){
 			$cookieStore.put('order',$scope.confirmOrder)
 		} else {
 			$scope.confirmOrder = $cookieStore.get('order')
+		}
+		// $scope.keys = Object.keys($scope.confirmOrder)
+
+		$scope.submitOrder = function(){
+			$scope.submitted = true;
+			var order = $scope.confirmOrder;
+			var orderForCreation ={
+				lineItems: order.lineItems,
+				sellerId: order.sellerId,
+				buyerId: order.buyerId,
+				status: 'offer'
+			}
+
+			offer.resource.save(orderForCreation,function(result){
+				toast('Successfully Submitted!',4000);
+				$cookieStore.remove('order');
+				offer.setOrder({});
+			},function(err){
+				if(err) {console.log('Error ',err)}
+			})
 		}
 		
 	})
