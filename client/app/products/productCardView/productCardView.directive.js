22b33e768da8d9ca0825a11980719bee4ad16a2a
David Chang <david.j.chang@gmail.com>
2015-02-17 12:19:05 -0500

    fixed

--- client/app/products/productCardView/productCardView.directive.js
+++ client/app/products/productCardView/productCardView.directive.js
@@ -26,16 +26,12 @@ angular.module('cornerfindApp')
                             });
                         }
                     }).then(function(){
-                        console.log('scope.product.Likes', scope.product.likes);
                         for(var i = 0; i < scope.product.likes.length; i++){
-                            console.log('el', scope.product.likes[i]);
                             // console.log('currentUser', scope.currentUser._id );
                             if (scope.product.likes[i].userId._id == scope.currentUser._id) {
-                                console.log('hit');
                                 scope.favorited = true;
                                 break;
                             } else {
-                                console.log('turnoff');
                                 scope.favorited = false;
                             }
                         }
@@ -76,19 +72,10 @@ angular.module('cornerfindApp')
                                 productid: scope.product._id,
                                 userid: scope.currentUser._id
                             }).$promise.then(function() {;
-                                // console.log('userLikes', scope.currentUser.likes);
-
-                                // console.log('productLikes',scope.product._id);
-                                // var userLikeIndex = scope.currentUser.likes.map(function(e) {
-                                //     return e.productId;
-                                // }).indexOf(scope.product._id);
-                                //  console.log('userLikeIndex', productLikeIndex);
-                                // scope.currentUser.likes.splice(userLikeIndex, 1);
 
                                 var productLikeIndex = scope.product.likes.map(function(e) {
                                     return e.userId._id;
                                 }).indexOf(scope.currentUser._id);
-                                console.log('productLikeIndex', productLikeIndex);
                                 scope.product.likes.splice(productLikeIndex, 1);
                             });
 
@@ -102,12 +89,6 @@ angular.module('cornerfindApp')
                                 productId: scope.product._id,
                                 userId: scope.currentUser._id
                             }).$promise.then(function(){
-                                //push like to current user
-                                // scope.currentUser.likes.push({
-                                //     productId: scope.product._id,
-                                //     userId: scope.currentUser._id
-                                // });
-                                // console.log('afterpush',scope.currentUser.likes )
                                 //push like to product likes. userId is actually User object to get username for text
                                 scope.product.likes.push({
                                     productId: scope.product._id,
